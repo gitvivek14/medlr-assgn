aa0625e2a08d06e4bae7c7a4e79387f0ef1842c8
gitvivek <varora2004@gmail.com>
2024-06-03 20:24:24 +0530

    done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,17 @@
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+
 export default function Home(){
   return(
-    <div>
-      Home
+    <div className="flex items-center justify-center text-3xl font-bold">
+      WELCOME TO MEDLR
+      <div>
+        <Link href="/home">
+        <Button>
+          Home
+        </Button>
+        </Link>
+      </div>
     </div>
   )
 }
