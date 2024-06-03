e752579d40a86c47c93c3c3b68521a1412b4881a
gitvivek <varora2004@gmail.com>
2024-06-03 20:18:01 +0530

    done

--- src/app/api/users/me/route.ts
+++ src/app/api/users/me/route.ts
@@ -1,19 +1,28 @@
-import { NextRequest,NextResponse } from "next/server";
-import User from "@/models/userModel";
-import { getDataFromToken } from "@/helpers/getDatafromtoken";
-import {connect} from "@/dbConfig/dbConfig"
+import { NextRequest, NextResponse } from 'next/server';
+import User from '@/models/userModel';
+import { getDataFromToken } from '@/helpers/getDatafromtoken';
+import { connect } from '@/dbConfig/dbConfig';
 
-connect()
-export async function GET(request:NextRequest){
+connect();
+
+export async function GET(request: NextRequest) {
     try {
-        const userid = await getDataFromToken(request)
-        const user = await User.findOne({_id:userid}).select("-password")
+        const userid = await getDataFromToken(request);
+        const user = await User.findOne({ _id: userid }).select('-password');
+        if (!user) {
+            return NextResponse.json({
+                message: 'User not found',
+            }, { status: 404 });
+        }
         return NextResponse.json({
-            message:"User Found",
-            data:user
-        },{status:200})
-    } catch (error:any) {
+            message: 'User found',
+            data: user,
+        }, { status: 200 });
+    } catch (error: any) {
         console.log(error);
-        
+        return NextResponse.json({
+            message: 'Failed to fetch user data',
+            error: error.message,
+        }, { status: 500 });
     }
 }
