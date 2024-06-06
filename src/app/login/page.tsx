ad4b350d6387db7ac0178f7e60268a6919b36ace
gitvivek <varora2004@gmail.com>
2024-06-06 08:17:20 +0530

    done

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,6 +1,7 @@
 "use client"
 import React,{useState,useEffect} from "react"
 import axios from "axios"
+import Link from "next/link";
 import  {useRouter} from  "next/navigation"
 import toast from "react-hot-toast"
 import { Input } from "@/components/ui/input"
@@ -92,6 +93,11 @@ export default function Page(){
                     buttondisabled ? "No Login":"Login"
                 }
             </button>
+            <div>
+                <Link href="/signup">
+                    Sign Up 
+                </Link>
+            </div>
             </form>
             </div>
     </div>
